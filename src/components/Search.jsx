ab2e2ab423d232ed230a5940d534ebad50b30df8
Tsveta Koleva <tzveta_miteva22@yahoo.com>
2019-03-09 22:17:47 +0200

    About page available

---
+++ src/components/Search.jsx
@@ -0,0 +1,6 @@
+{/* <form className="form-inline md-form form-sm active-cyan active-cyan-2">
+    <i className="fa fa-search" aria-hidden="true"></i>
+    <input className="form-control form-control-sm ml-3 w-75" type="text"
+        placeholder="Search by category..."
+        aria-label="Search" name="query" value="" />
+</form> */}
---
+++ src/views/About.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+
+function About() {
+    return (
+        <div>
+            <h2 className="allHeading">About My Travel Diary</h2>
+            <p className='aboutPar'>
+                The project is a single-page web application created using React.js library for the user interface and a local server build using Node.js and MongoDB for the back-end.
+            </p>
+            <p className='aboutPar'>
+                It is a kind of blog system where keen travelers can share their exciting adventures like travels to exotic destinations or amazing discoveries closer to home.
+            </p>
+        </div>
+    )
+}
+
+export default About;
