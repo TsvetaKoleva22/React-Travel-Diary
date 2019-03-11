c06350fbf116a82981681aca68db6c7f407f7cf5
Tsveta Koleva <tzveta_miteva22@yahoo.com>
2019-03-11 10:08:03 +0200

    Added description of the project. Changed Details by using smaller component.

--- src/views/Details.jsx
+++ src/views/Details.jsx
@@ -1,27 +1,19 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import DetailsSingleAdv from '../components/DetailsSingleAdv';
 
 function Details(props) {
     let arr = props.adventures.filter(adv => adv._id.toString() === props.match.params.advid.toString());
     let currAdv = arr[0];
-    let dateArr = currAdv.creationDate.substr(0, 10).split('-');
-    let year = dateArr[0];
-    let month = dateArr[1];
-    let day = dateArr[2];
-
-    let dateToShow = `${day}.${month}.${year}`;
 
     return (
         <div>
             <h2 className="allHeading">{currAdv.title}</h2>
             <div className="card col-6 contDet">
                 <img className="imgDet" src={currAdv.imageUrl} alt={currAdv.title} />
-                <div className="card-body">
-                    <h5 style={{ textAlign: 'center' }}>Destination: {currAdv.destination}</h5>
-                    <p className="card-text">{currAdv.description}</p>
-                    <p className="card-text">Author: {currAdv.author}</p>
-                    <p className="datePar">Added on: {dateToShow}</p>
-                </div>
+
+                <DetailsSingleAdv adv={currAdv}/>
+
                 <div className="card-footer" style={{ textAlign: 'center' }}>
                     {
                         sessionStorage.getItem('username') === currAdv.author ?
